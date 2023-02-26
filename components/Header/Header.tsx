e689a87f847d9e114fd70150af65218953ee7de3
Nelson Mello <nelson.melloo@hotmail.com>
2023-02-26 10:58:12 -0300

    teste

--- components/Header/Header.tsx
+++ components/Header/Header.tsx
@@ -1,10 +1,18 @@
 import React from 'react';
 
+import Image from 'next/image';
+
 const Header = () => {
   return (
-    <div>
-      <nav className="w-full">teste</nav>
-    </div>
+    <header className="flex-col justify-around pt-28">
+      <div role="img" aria-label="Logo do Sorteador" />
+      <Image
+        src="/imagens/participante.png"
+        alt="Participante com um presente na mão"
+        width={351}
+        height={117}
+      />
+    </header>
   );
 };
 
